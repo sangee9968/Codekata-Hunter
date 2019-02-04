44c095a6bbd2d477d04bb1bea9ceb10a5e8afb29
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-02-04 16:10:02 +0530

    Update h2.py

--- h2.py
+++ h2.py
@@ -1,5 +1,8 @@
 n=int(input())
 l=list(map(int,input().split()))
 l.sort(reverse=True)
-#result
-print(''.join(map(str,l)))
+if all(i==0 for i in l):
+    print("0")
+else:
+    #result
+    print(''.join(map(str,l)))
