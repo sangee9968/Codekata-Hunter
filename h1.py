a1dd96b77b02b90876b6729c4adb61bd8cabe800
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-02-04 15:56:32 +0530

    Update h1.py

--- h1.py
+++ h1.py
@@ -3,6 +3,8 @@ n=int(input())
 l=list(map(int,input().split()))
 l.sort()
 ml=list(set(l))
+if l==ml:
+        print("unique")
 for i in range(len(ml)):
         if (l.count(ml[i]) > 1 ):
           l1.append(ml[i])
