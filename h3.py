820ed955f7a683dbc47cdb86286c70ebea7b26af
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-02-09 04:10:25 -0800

    Update h3.py

--- h3.py
+++ h3.py
@@ -1,12 +1,11 @@
 n=int(input())
 c=0
+s=""
 l=list(map(int,input().split()))
 for i in range(0,len(l)):
     if i==l[i]:
-        s=s+str(i)+" "
-        c=c+1
-#result        
-print(s.strip()) 
+       print(''.join(str(i)),end=" ")
+       c=c+1
 if c==0:
     #result
     print("-1")
