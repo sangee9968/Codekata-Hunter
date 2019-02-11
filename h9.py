6dd7aa409b6b4dacdca7496cc6b2ec38b3f47cb7
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-02-11 16:18:54 +0530

    Update h9.py

--- h9.py
+++ h9.py
@@ -1,8 +1,12 @@
 n=int(input())
 l=list(map(int,input().split()))
-l1=[0]
+l1=[0,1]
 for i in range(0,len(l)):
   for j in range(i+1,len(l)):
-    if l[i]+l[j] in l1:
-      #result
+    if l[i]+l[j]==0:
       print(l[i],l[j])
+      break
+    elif l[i]<0 or l[i]<0:
+      if l[i]+l[j]==-1 or l[i]+l[j]==1:
+        print(l[i],l[j])  
+        break
