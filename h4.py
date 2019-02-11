fca5e2b2f98a0def0cc55306dd91582f8e935b49
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-02-11 15:29:58 +0530

    Update h4.py

--- h4.py
+++ h4.py
@@ -2,8 +2,8 @@ n=int(input())
 l=list(map(int,input().split()))
 l1=[]
 l1=list(set(l))
-for i in l:
-    for j in l1:
-       if l.count(j)==1:
-          print(i)
+for j in range(0,len(l1)):
+  if l.count(l1[j])==1:
+    #result
+    print(l1[j])
          
