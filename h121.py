5f878c8324660b44ad69bf3f78fdbd48629ecd16
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-15 09:58:33 +0530

    Update h121.py

--- h121.py
+++ h121.py
@@ -1,11 +1,9 @@
 s=input()
 l=[]
 l1=[]
-s1=""
 for i in range(0,len(s)+1):
 	for j in range(i+1,len(s)+1):
-		l.append(s[i:j])
-print(l)		
+		l.append(s[i:j])		
 for i in l:
 	if i[::-1]==i:
 		l1.append([i,len(i)])
