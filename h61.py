a79abe47af6afbe6c199c27b0f1882ed95ee0c51
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-17 08:33:26 +0530

    Update h61.py

--- h61.py
+++ h61.py
@@ -1,6 +1,11 @@
-n=input()
+n=int(input())
 l=list(map(int,input().split()))
 u,v=map(int,input().split())
-if u in l and v in l:
-  #result
-	print(abs(l.index(u)-l.index(v)))
+d=[]
+for i in range(n-1):
+	for j in range(i+1,n+1):
+		m=l[i:j]
+		if (m[0]==u and m[-1]==v) or (m[-1]==u and m[0]==v):
+			d.append(len(m))
+#result			
+print(min(d)-1)
