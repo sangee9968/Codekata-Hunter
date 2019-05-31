a927ea9d657cf9d29385e250afc430bcea890b67
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-31 19:06:19 +0530

    Update h67.py

--- h67.py
+++ h67.py
@@ -3,7 +3,6 @@ a=list(map(str,input().split("#")))
 b=list(map(str,input().split("#")))
 c1=list(int(a[i]) for i in range(1,len(a)))
 c2=list(int(b[i]) for i in range(1,len(b)))
-print(c1,c2)
 s1=sum(c1)
 s2=sum(c2)
 if s1>s2:
