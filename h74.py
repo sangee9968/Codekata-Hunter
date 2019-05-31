d13f5c796456be2549866053e3be27ee5e6628a3
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-31 12:31:33 +0530

    Create h74.py

---
+++ h74.py
@@ -0,0 +1,10 @@
+#input
+a = input("")
+b = input("")
+s = ""
+for i in range(len(a)):
+    for j in range(len(a),-1,-1):
+        if a[i:j] in b:
+            if len(a[i:j])>=len(s):
+                s=a[i:j]
+print(s)
