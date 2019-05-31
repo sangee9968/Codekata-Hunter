59dad78854bc254fa85de251b5daa715d455b1c1
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-05-31 18:48:04 +0530

    Update h63.py

--- h63.py
+++ h63.py
@@ -1,5 +1,4 @@
-#input
-n=int(input())
+#inputn=int(input())
 l=list(map(int,input().split()))
 l1=[]
 s=" "
@@ -7,4 +6,5 @@ for i in range(n-1):
 	c=l[i+1:]
 	l1.append(str(max(c)))
 l1.append("0")
-print(*l1))
+print(s.join(l1))
+
